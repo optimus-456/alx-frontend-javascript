47980121f51e46e599635d31fde7c3ee7ba0482b
Optimus <ijaolatoyosi03@gmail.com>
2024-12-13 20:25:29 +0100

    print teacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -12,3 +12,16 @@ interface Teacher {
 interface Directors extends Teacher {
   numberOfReports: number;
 }
+
+//Defining the printTeacherFunction
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+//Implementing the printTeacher functio
+const printTeacher: printTeacherFunction = (firstname, lastname) => {
+  return `${firstname}.${lastname}`;
+};
+
+// Example usage of the printTeacher function
+console.log(printTeacher("Arowosafe", "Eniola"));
